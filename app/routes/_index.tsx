e0fc41995edd3249620a0d492f11b803b0e59b4a
Yaroslav-Vasin <yaroslav.vasin@bystrov.agency>
2024-03-15 15:48:01 +0200

    core concept part one

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,33 +11,46 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    <main id="content" className="">
+      <h1>A better way to keeping track of your notes</h1>
+      <p>Try our early beta ond never loose track of your notes again!</p>
+      <p id="cta">
+        <Link to="/notes">Go to the demo page</Link>
+      </p>
+    </main>
+  )
 }
+
+
+// export default function Index() {
+//   return (
+//     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+//       <h1>Welcome to Remix</h1>
+//       <ul>
+//         <li>
+//           <a
+//             target="_blank"
+//             href="https://remix.run/tutorials/blog"
+//             rel="noreferrer"
+//           >
+//             15m Quickstart Blog Tutorial
+//           </a>
+//         </li>
+//         <li>
+//           <a
+//             target="_blank"
+//             href="https://remix.run/tutorials/jokes"
+//             rel="noreferrer"
+//           >
+//             Deep Dive Jokes App Tutorial
+//           </a>
+//         </li>
+//         <li>
+//           <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+//             Remix Docs
+//           </a>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// }
---
+++ app/routes/notes.tsx
@@ -0,0 +1,15 @@
+// export default const NotesPage = () => {
+//   return (
+//     <main>
+//       <h1>My notes</h1>
+//     </main>
+//   )
+// };
+
+export default function NotesPage() {
+  return (
+    <main>
+      <h1>My notes</h1>
+    </main>
+  )
+};
